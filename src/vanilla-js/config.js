6c33111fcdba4f6d7e7260116ad4e77f8625dd2f
NG3 <nico@properati.com>
2020-02-04 18:18:19 -0300

    textos acortados

--- src/vanilla-js/config.js
+++ src/vanilla-js/config.js
@@ -5,7 +5,7 @@ var config = {
     theme: 'light',
     alignment: 'left',
     title: '¿Cuáles son los mejores barrios para estudiantes?',
-    subtitle: 'Esto es lo que necesitás saber si vas a mudarte a Bogotá para ir a la U',
+    subtitle: 'Esto es lo que necesitas saber si vas a mudarte a Bogotá para ir a la U',
     byline: 'By Properati Data',
     footer: 'Source: source citations, etc.',
     chapters: [
@@ -13,7 +13,7 @@ var config = {
             id: 'intro',
             title: 'Comienza un nuevo ciclo',
             image: './images/student.jpg',
-            description: 'Con el nuevo ciclo, millones de estudiantes en todo el país retoman las clases. Cada año XX de estudiantes eligen Bogotá para vivir mientras estudian. Es por eso que Properati, el sitio de búsqueda de inmuebles líder de Latinoamérica, realizó este informe especial para conocer las mejores zonas de la ciudad para vivir y estudiar.',
+            description: 'Con el nuevo ciclo, millones de estudiantes retoman las clases. Es por eso que Properati, el sitio de búsqueda de inmuebles líder de Latinoamérica, realizó un informe especial para conocer las mejores zonas de la ciudad para vivir y estudiar.',
             location: {
                center: [-74.19341, 4.68313],
                zoom: 10.86,
@@ -59,7 +59,7 @@ var config = {
             id: 'universidades',
             title: 'Las universidades',
             image: './images/university.jpg',
-            description: 'En Bogotá existen 535 establecimientos universitarios explazados en distintas zonas de la ciudad..',
+            description: 'En Bogotá existen 535 establecimientos universitarios, emplazados en distintas zonas de la ciudad.',
             location: {
                center: [-74.19341, 4.68313],
                zoom: 10.86,
@@ -84,7 +84,7 @@ var config = {
             id: 'nucleo-universidades',
             title: 'La zona universitaria',
             image: './images/university_area.jpg',
-            description: 'Para cada instituto universitario, calculamos su área de influencia a 500 metros, la distancia que un estudio de la ONU señaló como cercana, para que los estudiantes puedan llegar cómodamente. En el centro, descubrimos una zona donde pueden unirse una gran cantidad de universidades, separadas cada una de ellas por menos de 500 metros de la más cercana, y la definimos como la "Zona Estudiantil"',
+            description: 'Descubrimos una Zona Estudiantil donde pueden unirse una gran cantidad de universidades, separadas cada una de ellas por menos de 500 metros de la más cercana',
             location: {
                center: [-74.10396, 4.63218],
                zoom: 12.86,
@@ -113,7 +113,7 @@ var config = {
             id: 'bares',
             title: 'Esparcimiento',
             image: './images/bar.jpg',
-            description: 'Pero sabemos que no todo puede ser estudio, es por eso que sumamos todos los bares y restaurantes de la ciudad, especialmente los que están dentro de la "Zona Estudiantil" para que tengas lo mejor de los dos mundos',
+            description: 'Pero no todo puede ser estudio, así que sumamos todos los bares y restaurantes de la ciudad, especialmente los que están dentro de la "Zona Estudiantil" para que tengas lo mejor de los dos mundos.',
             location: {
               center: [-74.08894, 4.62365],
               zoom: 13.21,
@@ -142,7 +142,7 @@ var config = {
             id: 'Mercado Inmobiliario',
             title: 'Arriendos',
             //image: './images/bar.jpg',
-            description: 'Lo más importante, por supuesto, es conseguir un lindo y económico apartamento para arrendar. Analizamos la oferta de apartamentos en arriendo en los barrios de la zona',
+            description: 'Lo más importante, por supuesto, es conseguir un lindo y económico apartamento. Analizamos la oferta de apartamentos en arriendo en los barrios de la zona.',
             location: {
              center: [-74.08380, 4.66274],
              zoom: 12.21,
@@ -204,8 +204,8 @@ var config = {
             id: 'la-catedral',
             title: 'La Catedral',
             //image: './images/bar.jpg',
-            description: 'La Catedral es un barrio ideal para quienes estén estudiando en Bogotá. Allí se encuentran 26 predios de educación superior y además cuenta con 15 bares y restaurantes ideales para el esparcimiento. Los arriendos  de apartamentos de 50 metros cuadrados tienen un valor promedio de $1,533,700 y el promedio de dormitorios de las unidades en oferta es de 1.3 ',
-            location: {
+            description: 'La Catedral es un barrio ideal para quienes estén estudiando en Bogotá. <br /> \b Establecimientos educativos: 6 <br />  \b Bares & Restaurantes: 15 <br /> \b Precio promedio del arriendo: $1,533,700 <br /> \b Promedio de dormitorios por apartamento: 1.2 ',
+             location: {
              center: [-74.07329, 4.59965],
              zoom: 17.13,
              pitch: 3.50,
@@ -226,7 +226,7 @@ var config = {
             id: 'sucre',
             title: 'Sucre',
             //image: './images/bar.jpg',
-            description: ' \b Precio promedio del arriendo: $1,483,550. <br /> \b Establecimientos educativos: 6 <br /> \b Bares & Restaurantes: 5 <br /> \b Promedio de dormitorios por apartamento: 1.2 ',
+            description: ' \b Precio promedio del arriendo: $1,483,550 <br /> \b Establecimientos educativos: 6 <br /> \b Bares & Restaurantes: 5 <br /> \b Promedio de dormitorios por apartamento: 1.2 ',
             location: {
              center: [-74.06561, 4.62967],
              zoom: 17.13,
